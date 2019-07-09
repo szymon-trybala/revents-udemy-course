bc5cf18422a8afaad08d050d8a3d3ee8ce058c52
Szymon Trybala <szymon.trybala@protonmail.com>
2019-07-09 02:08:04 +0200

    End of  "Thinking in React" section (S3)

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,12 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar/NavBar';
+import { Container } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
-        <h1>Re-vents</h1>
-      </div>
+      <Fragment>
+        <NavBar/>
+        <Container className="main">
+          <EventDashboard/>
+        </Container>
+      </Fragment> 
     );
   }
 }
 export default App;
+
