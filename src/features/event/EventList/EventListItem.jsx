aeb432e24a1d005f686929fc77900f87b272f9e1
Szymon Trybala <szymon.trybala@protonmail.com>
2019-07-10 01:41:28 +0200

    End of "Creating a CRUD app" section (S5)

--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -4,8 +4,8 @@ import EventListAttendee from './EventListAttendee';
 
 class EventListItem extends Component {
   render() {
-    const {event} = this.props;
-     return (
+    const { event, selectEvent, deleteEvent } = this.props;
+    return (
       <Segment.Group>
         <Segment>
           <Item.Group>
@@ -13,9 +13,7 @@ class EventListItem extends Component {
               <Item.Image size='tiny' circular src={event.hostPhotoURL} />
               <Item.Content>
                 <Item.Header>{event.title}</Item.Header>
-                <Item.Description>
-                  Hosted by {event.hostedBy}
-                </Item.Description>
+                <Item.Description>Hosted by {event.hostedBy}</Item.Description>
               </Item.Content>
             </Item>
           </Item.Group>
@@ -28,14 +26,28 @@ class EventListItem extends Component {
         </Segment>
         <Segment secondary>
           <List horizontal>
-            {event.attendees && event.attendees.map(attendee => (
-              <EventListAttendee key={attendee.id} attendee={attendee}/>
-            ))}
+            {event.attendees &&
+              event.attendees.map(attendee => (
+                <EventListAttendee key={attendee.id} attendee={attendee} />
+              ))}
           </List>
         </Segment>
         <Segment clearing>
-            <span>{event.description}</span>
-          <Button as='a' color='teal' floated='right' content='View' />
+          <span>{event.description}</span>
+          <Button
+            as='a'
+            onClick={() => selectEvent(event)}
+            color='teal'
+            floated='right'
+            content='View'
+          />
+          <Button
+            as='a'
+            color='red'
+            floated='right'
+            content='Delete'
+            onClick={() => deleteEvent(event.id)}
+          />
         </Segment>
       </Segment.Group>
     );
