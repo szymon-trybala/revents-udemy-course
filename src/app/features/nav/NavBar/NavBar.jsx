61ba66399af6dcac4760bc1bfb4669cc1bbb6954
Szymon Trybala <szymon.trybala@protonmail.com>
2019-07-27 23:02:05 +0200

    End of "Firebase authentication" section (S15)

--- src/app/features/nav/NavBar/NavBar.jsx
+++ src/app/features/nav/NavBar/NavBar.jsx
@@ -1,19 +1,19 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
+import { withFirebase } from 'react-redux-firebase';
 import { Menu, Container, Button } from 'semantic-ui-react';
 import { NavLink, Link, withRouter } from 'react-router-dom';
 import SignedOutMenu from '../Menus/SignedOutMenu';
 import SignedInMenu from '../Menus/SignedInMenu';
 import { openModal } from '../../modals/modalActions';
-import { logout } from '../../auth/authActions';
 
 const actions = {
-  openModal,
-  logout
+  openModal
 };
 
 const mapState = state => ({
-  auth: state.auth
+  auth: state.firebase.auth,
+  profile: state.firebase.profile
 });
 
 class NavBar extends Component {
@@ -26,13 +26,13 @@ class NavBar extends Component {
   };
 
   handleSignOut = () => {
-    this.props.logout();
+    this.props.firebase.logout();
     this.props.history.push('/');
   };
 
   render() {
-    const { auth } = this.props;
-    const authenticated = auth.authenticated;
+    const { auth, profile } = this.props;
+    const authenticated = auth.isLoaded && !auth.isEmpty;
 
     return (
       <Menu inverted fixed='top'>
@@ -62,7 +62,7 @@ class NavBar extends Component {
           {authenticated ? (
             <SignedInMenu
               signOut={this.handleSignOut}
-              currentUser={auth.currentUser}
+              profile={profile}
             />
           ) : (
             <SignedOutMenu
@@ -76,9 +76,9 @@ class NavBar extends Component {
   }
 }
 
-export default withRouter(
+export default withRouter(withFirebase(
   connect(
     mapState,
     actions
   )(NavBar)
-);
+));
--- src/app/features/user/Settings/SettingsDashboard.jsx
+++ src/app/features/user/Settings/SettingsDashboard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Grid } from 'semantic-ui-react';
 import SettingsNav from './SettingsNav';
 import { Route, Redirect, Switch } from 'react-router-dom';
@@ -6,24 +7,39 @@ import BasicPage from './BasicPage';
 import AboutPage from './AboutPage';
 import PhotosPage from './PhotosPage';
 import AccountPage from './AccountPage';
+import { updatePassword } from '../../auth/authActions';
 
-const SettingsDashboard = () => {
+const mapState = (state) => ({
+  providerId: state.firebase.auth.providerData[0].providerId
+})
+
+const actions = {
+  updatePassword
+};
+
+const SettingsDashboard = ({ updatePassword, providerId }) => {
   return (
     <Grid>
       <Grid.Column width={12}>
-      <Switch>
-        <Redirect exact from='/settings' to='/settings/basic'/>
-        <Route path='/settings/basic' component={BasicPage}/>
-        <Route path='/settings/about' component={AboutPage}/>
-        <Route path='/settings/photos' component={PhotosPage}/>
-        <Route path='/settings/account' component={AccountPage}/>
-      </Switch>
+        <Switch>
+          <Redirect exact from='/settings' to='/settings/basic' />
+          <Route path='/settings/basic' component={BasicPage} />
+          <Route path='/settings/about' component={AboutPage} />
+          <Route path='/settings/photos' component={PhotosPage} />
+          <Route
+            path='/settings/account'
+            render={() => <AccountPage updatePassword={updatePassword} providerId={providerId} />}
+          />
+        </Switch>
       </Grid.Column>
       <Grid.Column width={4}>
-        <SettingsNav/>
+        <SettingsNav />
       </Grid.Column>
     </Grid>
   );
 };
 
-export default SettingsDashboard;
+export default connect(
+  mapState,
+  actions
+)(SettingsDashboard);
