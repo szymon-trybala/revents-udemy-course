446e56ef9bd2a4750000039c0aab9216b90a7b2e
Szymon Trybala <szymon.trybala@protonmail.com>
2019-07-29 00:22:08 +0200

    End of "User management" section (S16)

---
+++ src/app/features/user/userActions.js
@@ -0,0 +1,17 @@
+import { toastr } from 'react-redux-toastr';
+
+export const updateProfile = user => async (
+  dispatch,
+  getState,
+  { getFirebase }
+) => {
+  const firebase = getFirebase();
+  const { isLoaded, isEmpty, ...updatedUser } = user;
+  console.log(updatedUser);
+  try {
+    await firebase.updateProfile(updatedUser);
+    toastr.success('Success', 'Your profile has been updated');
+  } catch (error) {
+    console.log(error);
+  }
+};
