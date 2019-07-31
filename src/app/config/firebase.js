1d65f9261ed9e3a8a203c6b132c9917ced7bfc6a
Szymon Trybala <szymon.trybala@protonmail.com>
2019-08-01 01:22:16 +0200

    End of "Photo Uploads" section (S17)

--- src/app/config/firebase.js
+++ src/app/config/firebase.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
   authDomain: 'revents-19a46.firebaseapp.com',
   databaseURL: 'https://revents-19a46.firebaseio.com',
   projectId: 'revents-19a46',
-  storageBucket: '',
+  storageBucket: 'revents-19a46.appspot.com',
   messagingSenderId: '1022639889138',
   appId: '1:1022639889138:web:21de8642b49b0aef'
 };
--- src/app/features/nav/Menus/SignedInMenu.jsx
+++ src/app/features/nav/Menus/SignedInMenu.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const SignedInMenu = ({ signOut, profile }) => {
   return (
     <Menu.Item position='right'>
-      <Image avatar spaced='right' src={profile.photoUrl || '/assets/user.png'} />
+      <Image avatar spaced='right' src={profile.photoURL || '/assets/user.png'} />
       <Dropdown pointing='top left' text={profile.displayName}>
         <Dropdown.Menu>
           <Dropdown.Item text='Create Event' icon='plus' />
