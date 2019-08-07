0b044b75c60ac873d9b7799ced0a25c78e421759
szymon-trybala <szymon.trybala@protonmail.com>
2019-08-07 17:18:42 +0200

    End of "Event chat system" section (S21)

--- src/app/features/event/eventActions.js
+++ src/app/features/event/eventActions.js
@@ -1,13 +1,13 @@
-import { FETCH_EVENTS } from './eventConstants';
+import { FETCH_EVENTS } from "./eventConstants";
 import {
   asyncActionStart,
   asyncActionFinish,
   asyncActionError
-} from '../async/asyncActions';
-import { fetchSampleData } from '../../data/mockApi';
-import { toastr } from 'react-redux-toastr';
-import { createNewEvent } from '../../common/util/helpers';
-import firebase from '../../../app/config/firebase';
+} from "../async/asyncActions";
+import { fetchSampleData } from "../../data/mockApi";
+import { toastr } from "react-redux-toastr";
+import { createNewEvent } from "../../common/util/helpers";
+import firebase from "../../../app/config/firebase";
 
 export const createEvent = event => {
   return async (dispatch, getState, { getFirestore, getFirebase }) => {
@@ -19,17 +19,17 @@ export const createEvent = event => {
     const newEvent = createNewEvent(user, photoURL, event);
 
     try {
-      let createdEvent = await firestore.add('events', newEvent);
+      let createdEvent = await firestore.add("events", newEvent);
       await firestore.set(`event_attendee/${createdEvent.id}_${user.uid}`, {
         eventId: createdEvent.id,
         userUid: user.uid,
         eventDate: event.date,
         host: true
       });
-      toastr.success('Success!', 'Event has been created');
+      toastr.success("Success!", "Event has been created");
       return createdEvent;
     } catch (error) {
-      toastr.error('Oops!', 'Something went wrong');
+      toastr.error("Oops!", "Something went wrong");
     }
   };
 };
@@ -40,9 +40,9 @@ export const updateEvent = event => {
 
     try {
       await firestore.update(`events/${event.id}`, event);
-      toastr.success('Success!', 'Event has been updated');
+      toastr.success("Success!", "Event has been updated");
     } catch (error) {
-      toastr.error('Oops!', 'Something went wrong');
+      toastr.error("Oops!", "Something went wrong");
     }
   };
 };
@@ -54,8 +54,8 @@ export const cancelToggle = (cancelled, eventId) => async (
 ) => {
   const firestore = getFirestore();
   const message = cancelled
-    ? 'Are you sure you want to cancel the event?'
-    : 'This will reactivate the event. Are you sure?';
+    ? "Are you sure you want to cancel the event?"
+    : "This will reactivate the event. Are you sure?";
   try {
     toastr.confirm(message, {
       onOk: async () =>
@@ -88,13 +88,13 @@ export const getEventsForDashboard = lastEvent => async (
 ) => {
   //let today = new Date();
   const firestore = firebase.firestore();
-  const eventsRef = firestore.collection('events');
+  const eventsRef = firestore.collection("events");
   try {
     dispatch(asyncActionStart());
     let startAfter =
       lastEvent &&
       (await firestore
-        .collection('events')
+        .collection("events")
         .doc(lastEvent.id)
         .get());
     let query;
@@ -102,12 +102,12 @@ export const getEventsForDashboard = lastEvent => async (
     lastEvent
       ? (query = eventsRef
           // .where('date', '>=', today)
-          .orderBy('date')
+          .orderBy("date")
           .startAfter(startAfter)
           .limit(2))
       : (query = eventsRef
           // .where('date', '>=', today)
-          .orderBy('date')
+          .orderBy("date")
           .limit(2));
 
     let querySnap = await query.get();
@@ -128,5 +128,30 @@ export const getEventsForDashboard = lastEvent => async (
   } catch (error) {
     console.log(error);
     dispatch(asyncActionError());
+  }
+};
+
+export const addEventComment = (eventId, values, parentId) => async (
+  dispatch,
+  getState,
+  { getFirebase }
+) => {
+  const firebase = getFirebase();
+  const profile = getState().firebase.profile;
+  const user = firebase.auth().currentUser;
+  let newComment = {
+    parentId: parentId,
+    displayName: profile.displayName,
+    photoURL: profile.photoURL || '/assets/user.png',
+    uid: user.uid,
+    text: values.comment,
+    date: Date.now()
+  }
+
+  try {
+    await firebase.push(`event_chat/${eventId}`, newComment);
+  } catch (error) {
+    console.log(error);
+    toastr.error("Oops", "Problem adding comment");
   }
 };
